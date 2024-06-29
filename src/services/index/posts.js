97ec697ac79365fd8fadd1411b9a50a52344bf24
ak <ak.personalprojects@gmail.com>
2024-06-30 01:42:37 +0300

    hardcoding backend link

--- src/services/index/posts.js
+++ src/services/index/posts.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getAllPosts = async (searchKeyword = "", page = 1, limit = 10) => {
   try {
     const { data, headers } = await axios.get(
-      `/api/posts?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`
+      `https://coauthor-testing-backend.onrender.com/api/posts?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`
     );
     return { data, headers };
   } catch (error) {
@@ -15,7 +15,9 @@ export const getAllPosts = async (searchKeyword = "", page = 1, limit = 10) => {
 
 export const getSinglePost = async ({ slug }) => {
   try {
-    const { data } = await axios.get(`/api/posts/${slug}`);
+    const { data } = await axios.get(
+      `https://coauthor-testing-backend.onrender.com/api/posts/${slug}`
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
@@ -32,7 +34,10 @@ export const deletePost = async ({ slug, token }) => {
       },
     };
 
-    const { data } = await axios.delete(`/api/posts/${slug}`, config);
+    const { data } = await axios.delete(
+      `https://coauthor-testing-backend.onrender.com/api/posts/${slug}`,
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
@@ -49,7 +54,11 @@ export const updatePost = async ({ updatedData, slug, token }) => {
       },
     };
 
-    const { data } = await axios.put(`/api/posts/${slug}`, updatedData, config);
+    const { data } = await axios.put(
+      `https://coauthor-testing-backend.onrender.com/api/posts/${slug}`,
+      updatedData,
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
@@ -66,7 +75,11 @@ export const createPost = async ({ token }) => {
       },
     };
 
-    const { data } = await axios.post(`/api/posts`, {}, config);
+    const { data } = await axios.post(
+      `https://coauthor-testing-backend.onrender.com/api/posts`,
+      {},
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
